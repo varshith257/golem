5fd1c47b446c2a9415fa8becaf82987bf2d9b40d
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2025-01-18 19:44:53 +0530

    fix!

--- golem/src/proxy.rs
+++ golem/src/proxy.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Reverse proxy implementation for Golem's single executable mode.
+/// Reverse proxy implementation for Golem's single executable mode.
 
 use anyhow::Context;
 use hyper::service::{make_service_fn, service_fn};
